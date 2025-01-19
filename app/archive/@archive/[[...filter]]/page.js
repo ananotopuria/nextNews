4e5f041301afaea4f5ccd94562aba2e0286476a9
Anano Topuria <anano.topuriaa@gmail.com>
2025-01-20 00:01:24 +0400

    feat: handle errors with error page

--- app/archive/@archive/[[...filter]]/page.js
+++ app/archive/@archive/[[...filter]]/page.js
@@ -19,9 +19,9 @@ export default function FilteredNewsPage({ params }) {
     news = getNewsForYear(selectedYear);
     links = getAvailableNewsMonths(selectedYear);
   }
-  if(selectedYear && selectedMonth){
-      news = getNewsForYearAndMonth(selectedYear, selectedMonth);
-      links = [];
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
   }
   let newsContent = <p>No news found for the selected period.</p>;
 
@@ -31,16 +31,28 @@ export default function FilteredNewsPage({ params }) {
 
   const newsYear = params.filter;
 
+  if (
+    (selectedYear && !getAvailableNewsYears().includes(+selectedYear)) ||
+    (selectedMonth &&
+      !getAvailableNewsYears(selectedYear).includes(+selectedMonth))
+  ) {
+    throw new Error("Invalid filter.");
+  }
+
   return (
     <>
       <header id="archive-header">
         <nav>
           <ul>
             {links.map((link) => {
-              const href = selectedYear ? `/archive/${selectedYear}/${link}` : `/archive/${link}`
-              return <li key={link}>
-                <Link href={href}>{link}</Link>
-              </li>;
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
             })}
           </ul>
         </nav>
